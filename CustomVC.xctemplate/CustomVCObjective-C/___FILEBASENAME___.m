6be3cecb37fa4f230e80e47091f8e314f7757b34
ifelseboyxx <jasonxineio@gmail.com>
2017-01-14 23:02:20 +0800

    Template add

---
+++ CustomVC.xctemplate/CustomVCObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,97 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+//
+//                                  _oo8oo_
+//                                 o8888888o
+//                                 88" . "88
+//                                 (| -_- |)
+//                                 0\  =  /0
+//                               ___/'==='\___
+//                             .' \\|     |// '.
+//                            / \\|||  :  |||// \
+//                           / _||||| -:- |||||_ \
+//                          |   | \\\  -  /// |   |
+//                          | \_|  ''\---/''  |_/ |
+//                          \  .-\__  '-'  __/-.  /
+//                        ___'. .'  /--.--\  '. .'___
+//                     ."" '<  '.___\_<|>_/___.'  >' "".
+//                    | | :  `- \`.:`\ _ /`:.`/ -`  : | |
+//                    \  \ `-.   \_ __\ /__ _/   .-` /  /
+//                =====`-.____`.___ \_____/ ___.`____.-`=====
+//                                  `=---=`
+//
+//                          佛祖保佑        永无bug
+
+
+#import "___FILEBASENAME___.h"
+// VC
+
+// Model
+
+// View
+
+// wheel
+
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+
+#pragma mark -  LifeCyle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+}
+
+- (void)dealloc
+{
+    NSLog(@"%@_____dealloc",self.class);
+}
+
+#pragma mark - UI Init
+
+#pragma mark - Configure Default Info
+
+#pragma mark - Target Methods
+
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+
+#pragma mark - OtherDelegate
+
+
+
+@end
