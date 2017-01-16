352ac92274f55bf102de81c98f9b600be004f5be
ifelseboyxx <jasonxineio@gmail.com>
2017-01-16 20:58:02 +0800

    Template Add

---
+++ CustomVC.xctemplate/CustomVCSwift/___FILEBASENAME___.swift
@@ -0,0 +1,34 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    
+    //MARK: - LifeCyle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    deinit {
+        print("\(object_getClassName(self)) - 释放了!")
+    }
+    
+    //MARK: - Intial Methods
+    
+    //MARK: - Target Methods
+    
+    //MARK: - Private Method
+    
+    //MARK: - Setter Getter Methods
+    
+    //MARK: - External Delegate
+    
+}
+
